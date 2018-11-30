0c8f8ef481cd6fb4eed7f48f710e43a961d5f43e
Dustin Bischke <dustin@bischke.ca>
2018-11-30 13:55:51 +0000

    Added Config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
     define('HOST', 'localhost');
     define('USER', 'root');
-    define('PASS', 'pass');
-    define('DB', 'db');
+    define('PASS', '');
+    define('DB', 'dustin');
     $conn = mysqli_connect(HOST, USER, PASS);
     define('USERNAME_REGEX', '/^[a-zA-Z0-9]{4,16}$/');
     define('PASSWORD_REGEX', '/^(?:(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W))[a-zA-Z0-9\S]{8,128}$/');
